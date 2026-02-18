87ee5ca24f0f042bc2a2aa94da84fe3c50a9575d
Edwin <edwin@exact.com>
2026-02-18 19:40:20 +0100

    Fix_tailwind_build_config_and_set_dev_port

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,6 @@
-const loadPlugin = async (name) => {
-  const mod = await import(name);
-  return mod.default ?? mod;
-};
-
-const normalizePlugin = (plugin) => {
-  if (typeof plugin === 'function' && !plugin.postcssPlugin) {
-    return plugin();
-  }
-  return plugin;
-};
-
-export default async () => {
-  let tailwindPlugin;
-  try {
-    tailwindPlugin = await loadPlugin('@tailwindcss/postcss');
-  } catch (error) {
-    tailwindPlugin = await loadPlugin('tailwindcss');
-  }
-
-  const autoprefixer = await loadPlugin('autoprefixer');
-
-  return {
-    plugins: [normalizePlugin(tailwindPlugin), normalizePlugin(autoprefixer)],
-  };
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
 };
